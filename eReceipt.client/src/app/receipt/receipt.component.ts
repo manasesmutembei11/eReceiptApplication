f2fb7bd5aa42996b058822ab1a9feb3bb55d49b7
Manases <manasesmutembei11@gmail.com>
2024-10-01 15:58:35 +0300

    Angular setup

--- eReceipt.client/src/app/receipt/receipt.component.ts
+++ eReceipt.client/src/app/receipt/receipt.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ReceiptService } from '../receipt.service';
-import { CommonModule } from '@angular/common';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
